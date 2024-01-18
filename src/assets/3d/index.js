dd2c81565f649d855f854f4e1c38ed91979af016
Noman Zawad <nomanzawad@gmail.com>
2024-01-18 22:03:18 +0600

    working island , plane, bird

---
+++ src/assets/3d/index.js
@@ -0,0 +1,7 @@
+import islandScene from "./island.glb";
+import skyScene from "./sky.glb";
+import birdScene from "./bird.glb";
+import placeScene from "./plane.glb";
+import foxScene from "./fox.glb";
+
+export { islandScene, skyScene, birdScene, placeScene, foxScene };
---
+++ src/models/Sky.jsx
@@ -0,0 +1,23 @@
+import { useGLTF } from "@react-three/drei";
+import skyScene from "../assets/3d/sky.glb";
+import { useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+
+const Sky = ({ isRotating }) => {
+  const sky = useGLTF(skyScene);
+  const skyref = useRef();
+
+  useFrame((_, delta) => {
+    if (isRotating) {
+      skyref.current.rotation.y += 0.15 * delta;
+    }
+  });
+
+  return (
+    <mesh ref={skyref}>
+      <primitive object={sky.scene} />
+    </mesh>
+  );
+};
+
+export default Sky;
